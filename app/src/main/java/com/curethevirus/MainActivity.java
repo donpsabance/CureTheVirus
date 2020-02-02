8c046fe0c8ab3494c75f0d73ea4197e74bb20cc1
donpsabance <donpsabance@gmail.co>
2020-02-01 23:08:32 -0800

    create main menu screen with rotating image

--- app/src/main/java/com/curethevirus/MainActivity.java
+++ app/src/main/java/com/curethevirus/MainActivity.java
@@ -2,7 +2,15 @@ package com.curethevirus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //full screen immersive experience - taken from android docs
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE |
+                View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        //animation for rotating icon
+        RotateAnimation rotateAnimation = new RotateAnimation(0f, 360f, 300f, 250f);
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+        rotateAnimation.setRepeatCount(Animation.INFINITE);
+        rotateAnimation.setDuration(7000);
+
+        final ImageView imageView = findViewById(R.id.mainIconView);
+        imageView.startAnimation(rotateAnimation);
     }
 }
