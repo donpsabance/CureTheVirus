16a5c8180cc7de09e17c6325c51e7d3d9fc14068
donpsabance <donpsabance@gmail.co>
2020-02-03 21:45:22 -0800

    added settings page

--- app/src/main/java/com/curethevirus/GameSettingsActivity.java
+++ app/src/main/java/com/curethevirus/GameSettingsActivity.java
@@ -2,13 +2,58 @@ package com.curethevirus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class GameSettingsActivity extends AppCompatActivity {
+
+    public static Intent makeIntent(Context context){
+        Intent intent = new Intent(context, GameSettingsActivity.class);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_settings);
+
+        //full screen immersive experience - taken from android docs
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE |
+                View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+
+        loadBackButton();
+        loadResetStatisticsButton();
+
+    }
+
+    private void loadBackButton(){
+
+        Button button = findViewById(R.id.settingsBackButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void loadResetStatisticsButton(){
+
+        Button button = findViewById(R.id.settingsResetButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
