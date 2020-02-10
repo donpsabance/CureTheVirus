4351deebe7ad1ad8564582c921c6047a3afd3b0c
Amos Ko <amosk@nugnuts.hitronhub.home>
2020-02-09 21:59:17 -0800

    edited help screen instructions. Made links clickable.

--- app/src/main/java/com/curethevirus/GameHelpActivity.java
+++ app/src/main/java/com/curethevirus/GameHelpActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class GameHelpActivity extends AppCompatActivity {
 
@@ -26,6 +28,11 @@ public class GameHelpActivity extends AppCompatActivity {
 
         //load button
         loadBackButton();
+
+        //make links clickable
+
+//        TextView t1 = findViewById(R.id.cmpt276);
+//        t1.setMovementMethod(LinkMovementMethod.getInstance());
 
     }
 
